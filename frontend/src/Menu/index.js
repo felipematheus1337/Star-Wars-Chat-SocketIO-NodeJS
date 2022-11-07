2b8ff0935bc069a7c41fad19589cccd636fcc5af
felipematheus1337 <lipehbr@gmail.com>
2022-11-06 23:14:11 -0300

    Frontend: changed the endpoint for backend

--- frontend/src/Menu/index.js
+++ frontend/src/Menu/index.js
@@ -11,7 +11,8 @@ import wallpapers from "../Chat/wallpapers.json";
 function Menu() {
     const navigate = useNavigate();
 
-    const URL_IO = "http://localhost:3001"
+    //const URL_IO = "http://localhost:3001"
+    const URL_IO = "https://star-wars-chat-socket-io-node-js-backend.vercel.app/"
 
     const [characters,setCharacters] = useState();
     const [renderSelect,setRenderSelect] = useState(false)
